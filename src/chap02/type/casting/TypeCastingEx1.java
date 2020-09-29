c550636d25183e950a89f6c440ba22354af1323c
sj1338 <id177@naver.com>
2020-09-29 16:34:31 +0900

    1

--- src/chap02/type/casting/TypeCastingEx1.java
+++ src/chap02/type/casting/TypeCastingEx1.java
@@ -1,34 +1,49 @@
 package chap02.type.casting;
 
 public class TypeCastingEx1 {
-
 	public static void main(String[] args) {
 		// primitive type 8개
-
-		// 1byte   2byte 4byte 8byte
-		// boolean char
-		// byte	   short int   long
-		//				 float double
 		
+		// 1byte   2byte 4byte 8byte
+		// boolean char  
+		// byte    short int   long
+		//               float double
 		
 		byte byteValue = 3;
 		short shortValue = 129;
 		int intValue = 1000000;
 		long longValue = 10000000000L;
 		
-		
-		// 자동 형변환 
-		shortValue = byteValue; // 얘가 더 큰 범위를 가지고 있어서 문제가없음
+		// 자동 형변환
+		shortValue = byteValue;
 		intValue = shortValue;
 		longValue = intValue;
-
 		
-		// 강제 형변환, 안전할때만 써야함
-		longValue = 230000000000L;
-		intValue = (int) longValue; // 문제가 생길수도있음
+		// 강제 형변환
+		longValue = 2300000000L;
+		intValue = (int) longValue;
 		System.out.println(intValue);
 		
-	
+		// 자동형변환 (정수->실수)
+		float floatValue = 0.0F;
+		double doubleValue = 0.0;
+		
+		doubleValue = intValue;
+		floatValue = intValue;
+		
+		longValue = 9223372036854775807L;
+		floatValue = longValue;
+		System.out.println(floatValue);
+		
+		doubleValue = longValue;
+		
+		// 강제 형변환 (실수->정수)
+		intValue = (int) floatValue;
+		
+		
 	}
-
 }
+
+
+
+
